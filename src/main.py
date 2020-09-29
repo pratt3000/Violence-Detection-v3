928019da3e9297b990d6cacc610a818889c819f5
pratt3000 <pratt3000@gmail.com>
2020-09-29 19:48:49 +0530

    main.py working perfectoo

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ detector.setModelTypeAsRetinaNet()
 detector.setModelPath(config.PRETRAINED_MODEL_PATH)
 detector.loadModel(detection_speed = config.OBJECT_DETECTION_SPEED) #change parameter to adjust accuracy and speed
 custom = detector.CustomObjects(person=True)
-cv2.namedWindow('result', cv2.WINDOW_AUTOSIZE)
+
 
 def get_boxes(frame):
     _, detections = detector.detectCustomObjectsFromImage(
@@ -22,10 +22,14 @@ def get_boxes(frame):
         )
     return detections
 
-def mask_frame(frame, detections):
+def mask_frame(frame, detections, detections_temp):
     mask = np.zeros(frame.shape, dtype=np.uint8)
 
     for eachObject in detections:
+        x1,y1,x2,y2 = eachObject["box_points"]
+        mask = cv2.rectangle(mask, (x1, y1),(x2,y2), (255,255,255), -1) 
+
+    for eachObject in detections_temp:
         x1,y1,x2,y2 = eachObject["box_points"]
         mask = cv2.rectangle(mask, (x1, y1),(x2,y2), (255,255,255), -1) 
 
@@ -40,12 +44,6 @@ def text_to_frame(frame, label):
 
     return frame
 
-def frame_preprocessing(frame):
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.resize(frame, dsize=(config.IMG_SIZE, config.IMG_SIZE), interpolation=cv2.INTER_CUBIC)
-
-    return frame
-
 def get_frame_difference(frame_1, frame_2):
     frame = cv2.absdiff(frame_1, frame_2)
     ret,thresh1 = cv2.threshold(frame,config.THRESHOLD_DIFF_TOLERANCE,255,cv2.THRESH_BINARY)   #127 is threshold
@@ -56,37 +54,37 @@ vidcap = cv2.VideoCapture(0)
 success = True
 frame_temp=np.zeros((config.IMG_SIZE,config.IMG_SIZE)).astype(np.uint8) 
 stacked_frames=[]
+detections_temp = []
 count=0
 
 while(success):
 
-    success, frame = vidcap.read()                          # get frame serially
+    success, frame = vidcap.read()                  # get frame serially
+    frame = cv2.resize(frame, dsize=(config.IMG_SIZE, config.IMG_SIZE), interpolation=cv2.INTER_CUBIC)
+
+    detections = get_boxes(frame)                   # get obj detection
     
-    detections = get_boxes(frame)                           # get obj detection
+    frame = mask_frame(frame, detections, detections_temp)          # mask is not needed
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    diff = get_frame_difference(frame, frame_temp)  # get difference btw frames
+    
+    # if count < config.FRAME_BATCH_SIZE:
+    #     stacked_frames.append(diff)
+    #     count += 1
+    # else:
+    #     stacked_frames=[]
+    #     count = 0
 
-    frame = frame_preprocessing(frame)                      # grayscale  
-    frame_diff = get_frame_difference(frame, frame_temp)    # get difference btw frames
-    frame = mask_frame(frame_diff, detections)              # mask is not needed
-
-    if count < config.FRAME_BATCH_SIZE:
-        stacked_frames.append(frame)
-        count += 1
-    else:
-        stacked_frames=[]
-        count = 0
     frame_temp = frame
+    detections_temp = detections
 
-    label = 'Violence'                                      #add CNN ka predict() here
-    frame = text_to_frame(frame, label)
+    label = 'Violence'                      #add CNN ka evaluate here
+    diff = text_to_frame(diff, label)
 
-    #frame = cv2.resize(frame, (960, 540))
-    cv2.imshow('result', frame)
+    frame = cv2.resize(frame, (960, 540))
+    cv2.imshow('result', diff)
     
     cv2.waitKey(1)
 
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
-
-cv2.waitKey()
-cv2.destroyAllWindows()
-cv2.waitKey() 
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
